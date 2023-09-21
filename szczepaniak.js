1d31a970600ead7383b5ee8ffb93307349b752c8
Matrixooo <113419124+Matrixooo@users.noreply.github.com>
2023-09-21 09:56:21 +0100

    Add files via upload

---
+++ szczepaniak.js
@@ -0,0 +1,2 @@
+document.write("Ostatnia modyfikacja strony".fontcolor("Gray").bold().fontsize(5)+"<br>");
+document.write(document.lastModified .fontcolor("Gray").bold() .strike() .fontsize(5)+"<br>");
